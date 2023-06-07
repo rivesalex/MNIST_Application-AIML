596cd936e1557fa0b6f05831f5c0fc64ecc5f5f7
rivesalex <rivesalex23@gmail.com>
2023-06-06 21:38:19 -0400

    added files

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Using the MNIST dataset to create an application capable of identifying any real
 Several algos that need to be created:
 1) grid resizing (Done)
 2) something to combine (not Done)
+3) Bring in the digits from the MNIST dataset
---
+++ src/train/train_data.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tensorflow as tf
+
+import tensorflow_datasets as tfds
+a = 10
+image, label = tfds.as_numpy(tfds.load(
+    'mnist',
+    split='test',
+    batch_size=-1,
+    as_supervised=True,
+))
+
+breakpoint()
+
